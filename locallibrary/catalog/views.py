d76dab1b6fe2b9fc0d789e5abf824d505d8cc6d2
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-08-10 21:10:52 +0200

    first version of catalog page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, BookInstance, Genre
+
+
+def catalog(request):
+    # View function for home page of site.
+
+    # Generate counts of some of the main objects
+    number_of_books = Book.objects.all().count()
+    number_of_instances = BookInstance.objects.all().count()
+
+    # Available books
+    number_of_available_instances = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    number_of_authors = Author.objects.count()
+
+    context = {
+        'number_of_books': number_of_books,
+        'number_of_instances': number_of_instances,
+        'number_of_available_instances': number_of_available_instances,
+        'number_of_authors': number_of_authors,
+    }
+
+    return render(request, 'catalog.html', context=context)
