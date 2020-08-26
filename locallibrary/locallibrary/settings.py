ef3787a2d0c062bf3cc8bc0d98ef01c1c1435a04
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-08-26 12:58:16 +0200

    settings variables from environment

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r%g&%&1314map6my$46ah0@y^x^-_$ojclruj4-et%u@&k1zgv'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'r%g&%&1314map6my$46ah0@y^x^-_$ojclruj4-et%u@&k1zgv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
