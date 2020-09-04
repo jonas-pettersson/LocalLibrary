c9863039c0db6e7752a1745bdbff9fd4baea71cd
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-04 23:50:13 +0200

    local host allowed

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG_VALUE', '') != 'False'
 
-ALLOWED_HOSTS = ['django-library-pettersson.herokuapp.com']
+ALLOWED_HOSTS = ['django-library-pettersson.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
